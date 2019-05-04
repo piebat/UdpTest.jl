31fcd36f364f1aecbfe375fd7eb02ca48dedbf83
Pietro Battistoni <pietro@battistoni.it>
2019-05-04 17:11:48 +0200

    Documentation added

--- docs/make.jl
+++ docs/make.jl
@@ -3,13 +3,16 @@ using UdpTest
 
 makedocs(
     sitename = "UdpTest",
-    format = :html,
+    format = Documenter.html(),
     modules = [UdpTest]
 )
 
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "https://github.com/piebat/UdpTest.jl.git"
+    branch = "gh-pages"
+    julia = "1.0.3"
+    osname = "linux"
+)
