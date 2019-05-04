10c7beb90aa9c578e79759b21c7b012a9d0a506d
Pietro Battistoni <pietro@battistoni.it>
2019-05-04 18:36:38 +0200

    Riprova

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,4 +4,4 @@ using Test, UdpTest
 #    @test "IPV4 127.0.0.1 : 1883 ==> uno" == show_info("uno", ["127.0.0.1","1883"],"IPV4")
     @test  0 == show_info("uno", ["127.0.0.1","1883"],"IPV4")
     @test_throws DomainError (-1)^0.5
-end
+end;
